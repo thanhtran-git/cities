4f0b73479b42200554050b4f4bcee57bc6999b96
Thanh Tran <85113171+thanhtran-git@users.noreply.github.com>
2024-01-18 17:50:54 +0100

    add margin

--- pages/cities/[slug].js
+++ pages/cities/[slug].js
@@ -6,7 +6,7 @@ import styled from "styled-components";
 
 const Card = styled.div`
   max-width: 600px;
-  margin: 0 auto;
+  margin: 100px auto;
   padding: 20px;
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
